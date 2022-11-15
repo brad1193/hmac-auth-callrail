5c37f53dc57b49dbd0d970750e52f02cc2c8462a
Brad Anderson <banderson@macbookair.lan>
2022-11-15 07:39:19 -0500

    first commit

--- hmac-auth-callrail/02-integration_spec.lua
+++ hmac-auth-callrail/02-integration_spec.lua
@@ -10,7 +10,7 @@ for _, strategy in helpers.all_strategies() do if strategy ~= "cassandra" then
 
     lazy_setup(function()
 
-      local bp = helpers.get_db_utils(strategy == "off" and "postgres" or strategy, nil, { PLUGIN_NAME })
+      local bp = helpers.get_db_utils(strategy == "off" and "postgres" or strategy, nil, { hmac-auth-callrail })
 
       -- Inject a test route. No need to create a service, there is a default
       -- service which will echo the request.
@@ -19,7 +19,7 @@ for _, strategy in helpers.all_strategies() do if strategy ~= "cassandra" then
       })
       -- add the plugin to test to the route we created
       bp.plugins:insert {
-        name = PLUGIN_NAME,
+        name = hmac-auth-callrail,
         route = { id = route1.id },
         config = {},
       }
@@ -31,7 +31,7 @@ for _, strategy in helpers.all_strategies() do if strategy ~= "cassandra" then
         -- use the custom test template to create a local mock server
         nginx_conf = "spec/fixtures/custom_nginx.template",
         -- make sure our plugin gets loaded
-        plugins = "bundled," .. PLUGIN_NAME,
+        plugins = "bundled," .. hmac-auth-callrail,
         -- write & load declarative config, only if 'strategy=off'
         declarative_config = strategy == "off" and helpers.make_yaml_file() or nil,
       }))
